2cb8f8ce40200c8f3f80a7c0061d40e010511709
Fabrice Kabongo <fabrice.k.kabongo@gmail.com>
2025-11-19 18:46:43 +0400

    test the assertion

--- world/location_test.go
+++ world/location_test.go
@@ -2,10 +2,11 @@ package world
 
 import (
 	"errors"
-	"github.com/hashicorp/go-uuid"
 	"log"
 	"math/rand/v2"
 	"testing"
+
+	"github.com/hashicorp/go-uuid"
 )
 
 func CreateRandomTestLocation() (*Location, error, string, string, float64, float64) {
@@ -28,6 +29,40 @@ func CreateRandomTestLocation() (*Location, error, string, string, float64, floa
 func TestLocation(t *testing.T) {
 	t.Parallel()
 
+	t.Run("SetNode", func(t *testing.T) {
+		t.Parallel()
+		t.Run("should set the node of the location", func(t *testing.T) {
+			t.Parallel()
+			loc, err, _, _, _, _ := CreateRandomTestLocation()
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if loc.Node != nil {
+				t.Fatalf("expected node to be nil")
+			}
+			node := &TreeNode{}
+			loc.SetNode(node)
+
+			if loc.Node != node {
+				t.Fatalf("expected node to be set")
+			}
+		})
+		t.Run("should panic if the node is nil", func(t *testing.T) {
+			t.Parallel()
+			defer func() {
+				if r := recover(); r == nil {
+					t.Fatalf("expected panic but did not get one")
+				}
+			}()
+			loc, err, _, _, _, _ := CreateRandomTestLocation()
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			loc.SetNode(nil)
+		})
+	})
 	t.Run("NewLocation", func(t *testing.T) {
 		t.Parallel()
 		t.Run("should return an error if the namespace id is missing", func(t *testing.T) {
